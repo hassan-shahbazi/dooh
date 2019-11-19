ac4ad843fb42b190b46a2798ac149f2b45e24e3e
Hassan Shahbazi <h-shahbazi@hotmail.com>
2019-11-19 12:27:13 +0200

    update port and url

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def mock_route():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
